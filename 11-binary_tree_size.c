29a0b68822c5b2caf9ffe4f5e2a71560bf6d124c
wassimbel <wassim.belhedi1@gmail.com>
2020-07-22 17:46:41 -0400

    betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,8 +8,9 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) +
+		binary_tree_size(tree->right) + 1);
 }
 
