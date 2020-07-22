0646e27bf2bf98170184f82f4d723d7e1805cb48
wassimbel <wassim.belhedi1@gmail.com>
2020-07-22 17:47:51 -0400

    up

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_depth - measures the depth of a binary tree
  * @tree: a pointer to the root node of the tree to measure the depth
- * @Return: depth of a tree or 0
+ * Return: depth of a tree or 0
  **/
 
 size_t binary_tree_depth(const binary_tree_t *tree)
