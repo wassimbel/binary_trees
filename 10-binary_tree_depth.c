3224c592418dd7b1a1642a240821a97687bb2695
wassimbel <wassim.belhedi1@gmail.com>
2020-07-22 17:53:36 -0400

    fix

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,10 +8,12 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t i = 0;
+
 	if (!tree || !(tree->parent))
 		return (0);
-	size_t i = binary_tree_depth(tree->parent) + 1;
-		return (i);
+	i = binary_tree_depth(tree->parent) + 1;
+	return (i);
 }
 
 
