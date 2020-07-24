1f859564cd51b8ac1c9dc55f486b8c6cae5a381f
wassimbel <wassim.belhedi1@gmail.com>
2020-07-24 16:30:10 -0400

    betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,8 +8,8 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (!node || !node->parent)
-                return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 	if (node == node->parent->left)
 		return (node->parent->right);
 	else
